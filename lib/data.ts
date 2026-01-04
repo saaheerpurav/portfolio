1d20eb381582a3221d0e79f785caff77fd24867a
Saaheer <saaheer.purav@gmail.com>
2026-01-04 22:56:34 +0530

    experience and project updates

--- lib/data.ts
+++ lib/data.ts
@@ -2,10 +2,11 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 
 import fyldrop from "@/public/fyldrop.png";
-import chatbot from "@/public/chatbot.png"
+import chatbot from "@/public/chatbot.png";
 import polartrace from "@/public/polartrace.png";
 import chat from "@/public/chat.png";
 import eduly from "@/public/eduly.png";
+import storeai from "@/public/storeai.png";
 
 export const links = [
   {
@@ -44,7 +45,8 @@ export const experiencesData = [
     company: "SmaxFusion",
     url: "https://internloom.com",
     location: "Manipal Institute of Technology",
-    description: "Worked on building the InternLoom (by SmaxFusion) website. Oversaw the development, coordinated communication and organized meetings.",
+    description:
+      "Worked on building the InternLoom (by SmaxFusion) website. Oversaw the development, coordinated communication and organized meetings.",
     icon: React.createElement(CgWorkAlt),
     date: "2024 - 2025",
   },
@@ -53,47 +55,68 @@ export const experiencesData = [
     company: "Fiverr",
     url: "https://fiverr.com",
     location: "Remote",
-    description: "Built and sold custom desktop applications to clients from the United States, France, United Kingdom and Poland for Windows, MacOS and Linux operating systems.",
+    description:
+      "Built and sold custom desktop applications to clients from the United States, France, United Kingdom and Poland for Windows, MacOS and Linux operating systems.",
     icon: React.createElement(CgWorkAlt),
     date: "2022 - Current",
-  }
+  },
 ] as const;
 
 export const projectsData = [
   {
+    title: "Store AI",
+    url: "https://store-ai.saaheerpurav.com/",
+    description:
+      "Built a WhatsApp based ecommerce platform with product discovery, order placement, and support through chat",
+    tags: ["FastAPI", "Langchain", "DynamoDB", "Twilio", "Whisper"],
+    imageUrl: storeai,
+  },
+  {
+    title: "EduLy",
+    url: "https://github.com/saaheerpurav/school-management",
+    description:
+      "Developed a school management app that facilitates communication and cooperation between a school administrator and teachers",
+    tags: ["Flutter", "Dart", "Firebase", "Android"],
+    imageUrl: eduly,
+  },
+  {
     title: "FylDrop",
     url: "https://file-transfer-66619.web.app",
-    description: "Built a desktop app to share files to any device connected on a local network wirelessly, simply by scanning a QR code",
+    description:
+      "Built a desktop app to share files to any device connected on a local network wirelessly, simply by scanning a QR code",
     tags: ["React", "Electron", "Express", "Firebase", "Paypal"],
     imageUrl: fyldrop,
   },
   {
     title: "Chatbot",
     url: "https://chatbot.saaheerpurav.com",
-    description: "Developed a chatbot using Flask and LlamaIndex. Integrable into any website as an iframe for seamless user experience.",
-    tags: ["Flask", "LlamaIndex", "Tailwind CSS", "Docker", "Google Cloud Run", "AWS S3"],
+    description:
+      "Developed a chatbot using Flask and LlamaIndex. Integrable into any website as an iframe for seamless user experience.",
+    tags: [
+      "Flask",
+      "LlamaIndex",
+      "Tailwind CSS",
+      "Docker",
+      "Google Cloud Run",
+      "AWS S3",
+    ],
     imageUrl: chatbot,
-  },
-  {
-    title: "PolarTrace",
-    url: "https://pdf-compare.vercel.app",
-    description: "Built a PDF comparison tool to compare PDF documents side-by-side instantly, highlighting the created, deleted and modified lines.",
-    tags: ["Flask", "Python", "React", "Firebase", "Vercel"],
-    imageUrl: polartrace,
-  },
-  {
-    title: "EduLy",
-    url: "https://github.com/saaheerpurav/school-management",
-    description: "Developed a school management app that facilitates communication and cooperation between a school administrator and teachers",
-    tags: ["Flutter", "Dart", "Firebase", "Android"],
-    imageUrl: eduly,
   },
   {
     title: "Realtime Chat",
     url: "https://realtime-web-chat.glitch.me",
-    description: "Built a real-time web chat application that enables seamless communication among users in chat rooms.",
+    description:
+      "Built a real-time web chat application that enables seamless communication among users in chat rooms.",
     tags: ["Flask", "Python", "MySQL", "Socket.IO", "HTML", "CSS"],
     imageUrl: chat,
+  },
+  {
+    title: "PolarTrace",
+    url: "https://pdf-compare.vercel.app",
+    description:
+      "Built a PDF comparison tool to compare PDF documents side-by-side instantly, highlighting the created, deleted and modified lines.",
+    tags: ["Flask", "Python", "React", "Firebase", "Vercel"],
+    imageUrl: polartrace,
   },
 ] as const;
 
@@ -119,33 +142,34 @@ export const skillsData = [
   "Python",
   "OpenAI",
   "OpenCV",
-  "LlamaIndex",
+  "Langchain",
   "Docker",
   "Selenium",
   "Pandas",
   "Flask",
+  "FastAPI",
   "Dart",
   "Flutter",
   "Framer Motion",
 ] as const;
 
-export const activityData=[
+export const activityData = [
   {
     title: "Head of Open Source",
     club: "CodeX Club",
     img: "/clubs/codex.png",
-    start: "Dec 2024",
+    start: "Dec 2024 - May 2025",
   },
   {
     title: "Head of Web Development",
     club: "MBOSC Club",
     img: "/clubs/mbosc.png",
-    start: "Nov 2024",
+    start: "Nov 2024 - May 2025",
   },
   {
     title: "Vice Head of IT",
     club: "RADAR Club",
     img: "/clubs/radar.png",
-    start: "Nov 2024",
+    start: "Nov 2024 - May 2025",
   },
 ] as const;
